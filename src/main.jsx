b5c7c42d9429d5f59d18ae0b5240a4b80a3d12ed
thesirkiller <marcosgabriel040@gmail.com>
2026-01-16 08:52:12 -0300

    Trigger rebuild to apply environment variables

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/global.css'
 
+// Triggering new build with environment variables configured
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
